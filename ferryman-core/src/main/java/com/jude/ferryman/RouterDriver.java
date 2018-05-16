dc78d98d8856021c0923c6b6baf8e9c70fc62508
Jude <973829691@qq.com>
2018-05-16 21:59:39 +0800

    fix Fragment host has been destroyed

--- ferryman-core/src/main/java/com/jude/ferryman/RouterDriver.java
+++ ferryman-core/src/main/java/com/jude/ferryman/RouterDriver.java
@@ -18,44 +18,44 @@ import static com.jude.ferryman.internal.router.Router.REQUEST_DATA;
 
 public class RouterDriver {
 
-    public static void startActivity(Context ctx, String url){
+    public static void startActivity(Context ctx, String url) {
         Intent intent = getIntent(ctx, FerrymanSetting.interceptUrl(url));
-        startIntent(ctx,intent,null);
+        startIntent(ctx, intent, null);
     }
 
-    public static void startActivity(Context ctx, String url, OnActivityResultListener listener){
+    public static void startActivity(Context ctx, String url, OnActivityResultListener listener) {
         Intent intent = getIntent(ctx, FerrymanSetting.interceptUrl(url));
-        startIntent(ctx,intent,listener);
+        startIntent(ctx, intent, listener);
     }
 
-    static void startActivityFromAPI(Context ctx, String url){
+    static void startActivityFromAPI(Context ctx, String url) {
         Intent intent = getIntent(ctx, FerrymanSetting.interceptAPI(url));
-        startIntent(ctx,intent,null);
+        startIntent(ctx, intent, null);
     }
 
-    static void startActivityFromAPI(Context ctx, String url, OnActivityResultListener listener){
+    static void startActivityFromAPI(Context ctx, String url, OnActivityResultListener listener) {
         Intent intent = getIntent(ctx, FerrymanSetting.interceptAPI(url));
-        startIntent(ctx,intent,listener);
+        startIntent(ctx, intent, listener);
     }
 
     private static Intent getIntent(Context context, String url) {
         Intent intent = FerrymanSetting.findRouter(url).start(context, url);
-        if (intent != null){
+        if (intent != null) {
             intent.putExtra(REQUEST_DATA, Url.parse(url));
-            if (!(context instanceof Activity)){
+            if (!(context instanceof Activity)) {
                 intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
             }
         }
         return intent;
     }
 
-    private static void startIntent(Context context,Intent intent,OnActivityResultListener listener){
-        if (intent == null){
+    private static void startIntent(Context context, Intent intent, OnActivityResultListener listener) {
+        if (intent == null) {
             return;
         }
-        if (context instanceof FragmentActivity && listener != null){
+        if (context instanceof FragmentActivity && listener != null && !((FragmentActivity) context).getSupportFragmentManager().isDestroyed()) {
             ActivityResultHooker.startHookFragment((Activity) context, intent, listener);
-        }else {
+        } else {
             context.startActivity(intent);
         }
     }
